3dc3796dfebaeb584d225579a2d7d14ae206bf8e
eddy setiawanto <82553591+eddysetiawanto@users.noreply.github.com>
2022-04-13 13:08:25 +0700

    Initial Author Routes

---
+++ models/author.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    }
+})
+
+
+module.exports = mongoose.model('Author', authorSchema)
